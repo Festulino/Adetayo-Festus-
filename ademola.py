ba7f8c87ffc01b7b9015a65b211ccbcaa9c5a4f6
Festulino <adetayofestus900@gmail.com>
2024-09-11 05:49:45 -0500

    Add files via upload

---
+++ ademola.py
@@ -0,0 +1,7 @@
+#Variable Declaration
+name = input("Enter your name: ")
+print("my name is",name)
+
+#Variable Redeclaration
+name = input("Enter your middle name: ")
+print("My middle name is", name)
